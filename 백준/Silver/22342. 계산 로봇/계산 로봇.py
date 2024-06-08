05319c5e4dc4cda594fc2fe603a949d23aad48ce
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-06-08 23:43:47 +0900

    [Silver I] Title: 계산 로봇, Time: 1920 ms, Memory: 523576 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.py
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -1,10 +1,12 @@
+import sys
+input = sys.stdin.readline
 M,N = map(int,input().split())
 # 저장, 출력
-dp = [[[0] for _ in range(N)] for _ in range(M)]
+dp = [[[0] for _ in range(2001)] for _ in range(2001)]
 arr = []
 for i in range(M):
-    
-    line = list(map(int,input()))
+
+    line = list(map(int,input().rstrip()))
     arr.append(line)
 
 max_save = 0
@@ -15,16 +17,20 @@ for i in range(N):
         # 첫줄인 경우 출력에 가중치만 저장
         if i == 0:
             dp[j][i] = arr[j][i]
+
         # 그 외는 입력값 + 가중치
         else:
-            # 인덱스 오류 방지
-            if j == 0:
+            # 왼쪽 줄의 위 중간 아래 칸 중 최대 출력값을 저장
+            if M == 1:
+                dp[j][i] = dp[j][i - 1]
+            elif j == 0:
                 dp[j][i] = max(dp[j][i-1], dp[j+1][i-1])
             elif j == M-1:
                 dp[j][i] = max(dp[j][i - 1], dp[j - 1][i - 1])
             else:
                 dp[j][i] = max(dp[j][i - 1], dp[j - 1][i - 1],dp[j + 1][i - 1])
-            max_save = max(max_save,dp[j][i])
+            max_save = max(max_save, dp[j][i])
             dp[j][i] += arr[j][i]
+
 
 print(max_save)
