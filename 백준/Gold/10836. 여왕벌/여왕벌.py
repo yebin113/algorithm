7342e3d25c823a0e5a2c5da690c5599c3bd02f82
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-06-11 17:29:52 +0900

    [Gold IV] Title: 여왕벌, Time: 5020 ms, Memory: 116020 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,32 +1,22 @@
-def print_arr(arr):
-    for j in range(M):
-        print(*arr[j])
-    print()
+import sys
+input = sys.stdin.readline
 
 M,N = map(int,input().split())
 
 arr = [[1] * M for _ in range(M)]
-
-# N일 동안
+larva = [1]*(2*M-1)
+# N일 동안 애벌레 자란거 적립
 for _ in range(N):
-    line = list(map(int,input().split()))
-    larva = [0]*line[0]+[1]*line[1]+[2]*line[2]
-    # print(larva)
-    visited = [[0] * M for _ in range(M)]
+    one,two,three = list(map(int,input().split()))
+    two += one
+    three += two
+    for i in range(2*M-1):
+        if one <= i < two:
+            larva[i] += 1
+        elif i >= two:
+            larva[i] += 2
 
-    for i in range(len(larva)):
-        if i < M:
-            arr[M-1-i][0] += larva[i]
-            visited[M - 1 - i][0] = larva[i]
-        else:
-            arr[0][i-M+1] += larva[i]
-            visited[0][i-M+1] = larva[i]
-    # print_arr(arr)
-    # print_arr(visited)
-    for i in range(1,M):
-        for j in range(1,M):
-            arr[i][j] += max(visited[i-1][j-1],visited[i-1][j],visited[i][j-1])
-            visited[i][j] = max(visited[i-1][j-1],visited[i-1][j],visited[i][j-1])
-
-print_arr(arr)
-
+# 왼쪽, 위쪽 줄을 토대로 전체 애벌레 결과 
+# 첫번째 숫자는 수열에서 가져오기, 2~M까지는 맨 윗줄의 수 가져오기(증가하는 수이기 때문에 맨 윗줄이 가장 큰 수임)
+for i in range(M):
+    print(larva[M-1-i],*larva[M:])
